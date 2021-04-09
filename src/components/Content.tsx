4fd0be467012dadc384ef2364938dbc439d85a69
Claudio Santos <claudio@sistnet.com.br>
2021-04-09 00:53:04 -0300

    desafio realizado

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,39 @@
-export function Content() {
-  // Complete aqui
+import { Component } from "react";
+import { MovieCard } from "./MovieCard";
+
+import '../styles/content.scss'
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
 }
+class Content extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render(){
+    return (
+      <div className="container">
+        <header>
+          <span className="category">Categoria:<span> {this.props.selectedGenre.title}</span></span>
+        </header>
+
+        <main>
+          <div className="movies-list">
+            {this.props.movies.map(movie => (
+              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+            ))}
+          </div>
+        </main>
+      </div>
+    )
+  }
+}
+
+export default Content;
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,37 @@
-export function SideBar() {
-  // Complete aqui
+import { Component } from 'react';
+import { Button } from './Button';
+import '../styles/sidebar.scss';
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
 }
+
+class SideBar extends Component {
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return(
+      <nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+
+        <div className="buttons-container">
+          {this.props.genres.map(genre => (
+            <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => this.props.handleClickButton(genre.id)}
+              selected={this.props.selectedGenreId === genre.id}
+            />
+          ))}
+        </div>
+      </nav>
+    )
+  }
+}
+
+export default SideBar;
